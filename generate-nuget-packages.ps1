b44d1f0a83d735c9f52fa66acedcb80d1cf4ad65
Johan van Rensburg <jhnvnrnsbrg@gmail.com>
2018-08-22 19:00:38 +0200

    Update console to single target .NET Core 2.1 and nuget pack script

--- generate-nuget-packages.ps1
+++ generate-nuget-packages.ps1
@@ -1,5 +1,2 @@
 cd InheritDoc
-..\packages\NuGet.CommandLine.4.4.1\tools\NuGet.exe pack InheritDoc.csproj -Prop Configuration=Release -OutputDirectory \NuGet.local
-
-cd ..\InheritDocLib
-..\packages\NuGet.CommandLine.4.4.1\tools\NuGet.exe pack InheritDocLib.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory \NuGet.local
+dotnet pack -c Release -o \NuGet.local

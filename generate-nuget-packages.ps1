78fd2c44e9254dfaa4d93987b253519bca371446
Johan van Rensburg <jhnvnrnsbrg@gmail.com>
2018-08-23 15:05:07 +0200

    Add GlobalTool build condition

--- generate-nuget-packages.ps1
+++ generate-nuget-packages.ps1
@@ -1,2 +1,3 @@
-cd InheritDoc
-dotnet pack -c Release -o \NuGet.local
+dotnet pack -c Release -o \NuGet.local .\InheritDoc
+dotnet pack -c Release -o \NuGet.local .\InheritDoc /p:GlobalTool=true
+dotnet pack -c Release -o \NuGet.local .\InheritDocLib
